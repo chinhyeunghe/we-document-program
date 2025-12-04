93264681486b367a45b8a46048d7f5f4e2ea5650
chinhyeunghe <chinhdd.ph28756@gmail.com>
2025-12-04 17:23:08 +0700

    push component table vue js

--- docs/.vuepress/.temp/pages/vuejs/component/table.html.js
+++ docs/.vuepress/.temp/pages/vuejs/component/table.html.js
@@ -1,5 +1,5 @@
 import comp from "D:/laragon/www/document-program/docs/.vuepress/.temp/pages/vuejs/component/table.html.vue"
-const data = JSON.parse("{\"path\":\"/vuejs/component/table.html\",\"title\":\"DataTable Component - Tài liệu đầy đủ\",\"lang\":\"vi-VN\",\"frontmatter\":{},\"git\":{},\"filePathRelative\":\"vuejs/component/table.md\"}")
+const data = JSON.parse("{\"path\":\"/vuejs/component/table.html\",\"title\":\"DataTable Component - Tài liệu đầy đủ\",\"lang\":\"vi-VN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764843708000,\"contributors\":[{\"name\":\"chinhyeunghe\",\"username\":\"chinhyeunghe\",\"email\":\"chinhdd.ph28756@gmail.com\",\"commits\":1,\"url\":\"https://github.com/chinhyeunghe\"}],\"changelog\":[{\"hash\":\"83af4667143961ef8244bc0413f949c42e862648\",\"time\":1764843708000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"push component table vue js\"}]},\"filePathRelative\":\"vuejs/component/table.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -60,7 +60,7 @@ export default defineUserConfig({
         {
           text: "VueJs",
           collapsible: true,
-          children: ["/vuejs/info.md"],
+          children: ["/vuejs/info.md", "/vuejs/component/table.md"],
         },
       ],
     },
