f77c7c2ad5eaa95151c79eac80fb98e51edd3cbf
chinhyeunghe <chinhdd.ph28756@gmail.com>
2025-12-04 16:52:06 +0700

    done

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,14 @@
 export const siteData = JSON.parse("{\"base\":\"/we-document-program/\",\"lang\":\"vi-VN\",\"title\":\"Tài liệu lập trình\",\"description\":\"Website tài liệu lập trình\",\"head\":[],\"locales\":{}}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateSiteData) {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ siteData }) => {
+    __VUE_HMR_RUNTIME__.updateSiteData(siteData)
+  })
+}
--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,3 +1,16 @@
 import comp from "D:/laragon/www/document-program/docs/.vuepress/.temp/pages/index.html.vue"
 const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"vi-VN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764840325000,\"contributors\":[{\"name\":\"chinhyeunghe\",\"username\":\"chinhyeunghe\",\"email\":\"chinhdd.ph28756@gmail.com\",\"commits\":12,\"url\":\"https://github.com/chinhyeunghe\"}],\"changelog\":[{\"hash\":\"977765952f539eba86ebd3978641213dc40c56cc\",\"time\":1764840325000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done\"},{\"hash\":\"4522981e0720986f2ce9127b26a336a5cba8606c\",\"time\":1764840134000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done\"},{\"hash\":\"28438b85e0af81213fb43fb9067e489ecbf08086\",\"time\":1764839689000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"eabd39a33ef60197120c31b071338f05e190b286\",\"time\":1764839503000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"e749ea6ee88154c0898c18a065464fb1f0e0b9a8\",\"time\":1764839218000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"2e49d952723363ac25d75d76bbcffb4b62be8ef0\",\"time\":1764839091000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"4aa8eee879e402becc70e1b73cccd75ecee8ef72\",\"time\":1764838995000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"0365fe31100bf2d56b41371314d413e6cddc306c\",\"time\":1764838905000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"c5b4f0e83899d040835ef26de3e9d1b17f15a0b9\",\"time\":1764838762000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"8101b39944716306efff21c4741e524d991af9de\",\"time\":1764838680000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"bee3e349cd01847d8f87927f72eb798b8e2f17ab\",\"time\":1764838153000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done update\"},{\"hash\":\"394d881b28613968c9400884c595d4a346d86e9e\",\"time\":1764836967000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
