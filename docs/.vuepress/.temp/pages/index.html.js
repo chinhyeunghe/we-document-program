8577455237eceaf46588c620362f24f8684a8963
chinhyeunghe <chinhdd.ph28756@gmail.com>
2025-12-04 15:32:40 +0700

    done build

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/laragon/www/document-program/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Trang chủ\",\"lang\":\"vi-VN\",\"frontmatter\":{\"home\":true,\"title\":\"Trang chủ\",\"heroText\":\"Tài liệu lập trình\",\"tagline\":\"Xin chào những người anh em lập trình iu quí. Chúng ta hãy cùng nhau xây dựng và phát triển mạnh mẽ trên website chia sẻ tài liệu, kiến thức lập trình này nhé!\",\"actions\":[{\"text\":\"Khám phá Javascript\",\"link\":\"/javascript/intro\",\"type\":\"primary\"},{\"text\":\"Khám phá PHP\",\"link\":\"/php/intro\",\"type\":\"secondary\"}],\"features\":[{\"title\":\"JavaScript\",\"details\":\"Cùng nhau chia sẻ kiến thức Js\"},{\"title\":\"PHP\",\"details\":\"Cùng nhau chia sẻ kiến thức PHP\"},{\"title\":\"Vue js\",\"details\":\"Cùng nhau chia sẻ kiến thức Vue js\"}]},\"git\":{},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Trang chủ\",\"lang\":\"vi-VN\",\"frontmatter\":{\"home\":true,\"title\":\"Trang chủ\",\"heroText\":\"Tài liệu lập trình\",\"tagline\":\"Xin chào những người anh em lập trình iu quí. Chúng ta hãy cùng nhau xây dựng và phát triển mạnh mẽ trên website chia sẻ tài liệu, kiến thức lập trình này nhé!\",\"actions\":[{\"text\":\"Khám phá Javascript\",\"link\":\"/javascript/intro\",\"type\":\"primary\"},{\"text\":\"Khám phá PHP\",\"link\":\"/php/intro\",\"type\":\"secondary\"}],\"features\":[{\"title\":\"JavaScript\",\"details\":\"Cùng nhau chia sẻ kiến thức Js\"},{\"title\":\"PHP\",\"details\":\"Cùng nhau chia sẻ kiến thức PHP\"},{\"title\":\"Vue js\",\"details\":\"Cùng nhau chia sẻ kiến thức Vue js\"}]},\"git\":{\"updatedTime\":1764836967000,\"contributors\":[{\"name\":\"chinhyeunghe\",\"username\":\"chinhyeunghe\",\"email\":\"chinhdd.ph28756@gmail.com\",\"commits\":1,\"url\":\"https://github.com/chinhyeunghe\"}],\"changelog\":[{\"hash\":\"394d881b28613968c9400884c595d4a346d86e9e\",\"time\":1764836967000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/javascript/closure.html.js
+++ docs/.vuepress/.temp/pages/javascript/closure.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/laragon/www/document-program/docs/.vuepress/.temp/pages/javascript/closure.html.vue"
-const data = JSON.parse("{\"path\":\"/javascript/closure.html\",\"title\":\"Closure trong JavaScript\",\"lang\":\"vi-VN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764832797000,\"contributors\":[{\"name\":\"chinhyeunghe\",\"username\":\"chinhyeunghe\",\"email\":\"chinhdd.ph28756@gmail.com\",\"commits\":1,\"url\":\"https://github.com/chinhyeunghe\"}],\"changelog\":[{\"hash\":\"8b2c1f30c7454fb51f40da187edb975f2c989765\",\"time\":1764832797000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"Hello project document program\"}]},\"filePathRelative\":\"javascript/closure.md\"}")
+const data = JSON.parse("{\"path\":\"/javascript/closure.html\",\"title\":\"Closure trong JavaScript\",\"lang\":\"vi-VN\",\"frontmatter\":{},\"git\":{\"updatedTime\":1764836967000,\"contributors\":[{\"name\":\"chinhyeunghe\",\"username\":\"chinhyeunghe\",\"email\":\"chinhdd.ph28756@gmail.com\",\"commits\":2,\"url\":\"https://github.com/chinhyeunghe\"}],\"changelog\":[{\"hash\":\"394d881b28613968c9400884c595d4a346d86e9e\",\"time\":1764836967000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"done push project\"},{\"hash\":\"8b2c1f30c7454fb51f40da187edb975f2c989765\",\"time\":1764832797000,\"email\":\"chinhdd.ph28756@gmail.com\",\"author\":\"chinhyeunghe\",\"message\":\"Hello project document program\"}]},\"filePathRelative\":\"javascript/closure.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
