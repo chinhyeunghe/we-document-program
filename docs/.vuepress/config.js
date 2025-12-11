b8b2c544b75a5dff64a771e864ebadb3ff59eb71
chinhyeunghe <chinhdd.ph28756@gmail.com>
2025-12-11 11:16:21 +0700

    upload page component Confirm

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -60,7 +60,7 @@ export default defineUserConfig({
         {
           text: "VueJs",
           collapsible: true,
-          children: ["/vuejs/info.md", "/vuejs/component/table.md"],
+          children: ["/vuejs/info.md", "/vuejs/component/table.md", "/vuejs/component/confirm.md", ],
         },
       ],
     },
