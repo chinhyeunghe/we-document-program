63685cce7e52031be77506f642c3b27391de14bd
Vu Diep <vudiep621@gmail.com>
2026-02-09 09:45:45 +0700

    add compoment in menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -64,7 +64,7 @@ export default defineUserConfig({
         {
           text: "VueJs",
           collapsible: true,
-          children: ["/vuejs/info.md", "/vuejs/component/table.md", "/vuejs/component/confirm.md", "/vuejs/component/InputDate", "/vuejs/component/Button", "/vuejs/component/AutoRenderCodeInput"],
+          children: ["/vuejs/info.md", "/vuejs/component/table.md", "/vuejs/component/confirm.md", "/vuejs/component/InputDate", "/vuejs/component/Button", "/vuejs/component/AutoRenderCodeInput", "/vuejs/MetricCards.md", "/vuejs/InputMoney.md"],
         },
       ],
        "/warehouse-icon/": [
